6e68d2903dacd2b6e2a1145db29481c2ba81ecf7
benarito <bentric@yahoo.com>
2014-07-05 20:42:10 +0300

    Guessing game: added tests

--- guessing/tests.py
+++ guessing/tests.py
@@ -1,15 +1,26 @@
 import ptree.test
 import guessing.views as views
 from guessing.utilities import ParticipantMixIn, ExperimenterMixIn
+import random
 
 
 class ParticipantBot(ParticipantMixIn, ptree.test.ParticipantBot):
 
     def play(self):
-        pass
+
+        # start game
+        self.submit(views.Introduction)
+
+        # make your guess
+        self.submit(views.Guess, {"guess_value": random.choice(range(0, 100))})
+
+        self.submit(views.Results)
+
+    def play_p2(self):
+        self.submit(views.Guess, {"guess_value": random.choice(range(0, 100))})
 
 
 class ExperimenterBot(ExperimenterMixIn, ptree.test.ExperimenterBot):
 
     def play(self):
-        pass
+        self.submit(views.Experimenter)
