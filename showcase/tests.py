b1addc6ceb734a7da132725c5f6ded9632548e12
benarito <bentric@yahoo.com>
2014-08-03 16:42:01 +0300

    showcase: added tests

--- showcase/tests.py
+++ showcase/tests.py
@@ -6,4 +6,21 @@ from showcase.utilities import Bot
 class ParticipantBot(Bot):
 
     def play(self):
-        pass
+
+        self.submit(views.Introduction)
+
+        self.submit(views.FormsDemo, {
+            'demo_field1': 'yes',
+            'demo_field2': 'demo text',
+            'demo_field3': 'sample text',
+            'demo_field4': 'accept',
+            'demo_field5': 3,
+        })
+
+        self.submit(views.EmbedDemo)
+
+        self.submit(views.BootstrapWidgetDemo)
+
+        self.submit(views.AdminDemo)
+
+        self.submit(views.Results)
