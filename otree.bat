240bfdee154d59c3829c14ff80c48abc36438228
Gregor Müllegger <gregor@muellegger.de>
2014-10-09 09:22:45 +0200

    Adding otree.bat script.

---
+++ otree.bat
@@ -0,0 +1,13 @@
+:: A wrapper around the ./otree script for windows
+::
+:: On Windows open the `cmd` command line terminal, then go to the directory
+:: that this file is in with the `cd` command, e.g.
+::
+::   cd C:\Projects\oTree
+::
+:: Then type:
+::
+::   otree help
+::
+
+@python otree %*
