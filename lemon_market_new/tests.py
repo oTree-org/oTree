1f0cc20f8ab4f74011ed1aef355788384f43392a
Benson Njogu <bentric@yahoo.com>
2014-10-10 09:54:22 +0300

    removed duplicated apps

--- lemon_market_new/tests.py
+++
@@ -1,9 +0,0 @@
-from __future__ import division
-import lemon_market_new.views as views
-from lemon_market_new._builtin import Bot
-from otree.common import Money, money_range
-
-class PlayerBot(Bot):
-
-    def play(self):
-        pass
