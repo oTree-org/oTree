b86abd8f9f2d70b72172c148edfc503234e8c57d
Benson Njogu <bentric@yahoo.com>
2014-10-15 14:07:31 +0300

    asset market: more changes

--- asset_market/models.py
+++ asset_market/models.py
@@ -53,22 +53,32 @@ class Group(otree.models.BaseGroup):
     def set_transaction(self):
         for p in self.get_players():
             if (p.sp != 0 and p.sn != 0) or (p.bp != 0 and p.bn != 0):
-                if p.role() == 'Buyer':
+                # FIXME consider the following conditions.
+                '''
+                 -A transaction is only possible when one participant wants to buy and the other to sell.
+                 -Moreover, the submitted maximum buying price (BP) has to be higher than the minimum selling price (SP).
+                 -When this is the case, transaction takes place at the average of these two prices.
+                  That is transaction price (P) = 0.5*(BP+SP).
+                 -The number of shares traded (N) equals the smaller of the two numbers, i.e. N is the minimum of BN and SN.
+                 -If transaction takes place, N shares are removed from the selling party to the buying party; accordingly,
+                 cash equal to the transaction price (P) is added to the selling party and subtracted from the buying party.
+                 -Otherwise, no transaction takes place and the orders submitted, if any, are not revealed to participants.
+                '''
 
-                    # set transaction price
-                    self.transaction_price = 0.5*(p.bp+p.other_player().sp)
-                    self.shares_traded = min(p.bn, p.other_player().sn)
+                # set transaction price
+                self.transaction_price = 0.5*(p.bp+p.other_player().sp)
+                self.shares_traded = min(p.bn, p.other_player().sn)
 
-                    # adjust shares and cash
-                    amount = self.transaction_price * self.shares_traded
-                    if amount <= p.cash:
-                        # buyer
-                        p.shares += self.shares_traded
-                        p.cash -= self.transaction_price * self.shares_traded
-                        # seller
-                        p.other_player().shares -= self.shares_traded
-                        p.other_player().cash += self.transaction_price * self.shares_traded
-                        self.is_transaction = True
+                # adjust shares and cash
+                amount = self.transaction_price * self.shares_traded
+                if amount <= p.cash:
+                    # buyer
+                    p.shares += self.shares_traded
+                    p.cash -= self.transaction_price * self.shares_traded
+                    # seller
+                    p.other_player().shares -= self.shares_traded
+                    p.other_player().cash += self.transaction_price * self.shares_traded
+                    self.is_transaction = True
             else:
                 # no transaction
                 pass
@@ -119,18 +129,16 @@ class Player(otree.models.BasePlayer):
         """Returns other player in group. Only valid for 2-player groups."""
         return self.get_others_in_group()[0]
 
-    def role(self):
-        # TODO randomize player roles in each round
-        if self.id_in_group == 1:
-            return 'Seller'
-        if self.id_in_group == 2:
-            return 'Buyer'
-
     QUESTION_1_CHOICES = ['P=3, N=2','P=2, N=3','P=2.5, N=3','P=2.5, N=2','No transaction will take place',]
     QUESTION_2_CHOICES = ['$8, $12', '$12, $8', '$8, $8', '$12, $12', '$10, $10']
 
     understanding_question_1 = models.CharField(max_length=100, null=True, choices=QUESTION_1_CHOICES, verbose_name='', widget=widgets.RadioSelect())
     understanding_question_2 = models.CharField(max_length=100, null=True, choices=QUESTION_2_CHOICES, verbose_name='', widget=widgets.RadioSelect())
+
+    feedbackq = models.CharField(null=True, verbose_name='How well do you think this sample game was implemented?', widget=widgets.RadioSelectHorizontal())
+
+    def feedbackq_choices(self):
+        return ['Very well', 'Well', 'OK', 'Badly', 'Very badly']
 
     # check correct answers
     def is_understanding_question_1_correct(self):
