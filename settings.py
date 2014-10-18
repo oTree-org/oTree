72f2f4336fdae12625fe6782dddb3524e4f9656c
wickens <c.wickens@gmail.com>
2014-10-18 19:01:20 +0200

    removed the need for an env var on heroku, and configuring Travis to use DATABASE_URL

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 import os
 import otree.settings
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -17,18 +18,13 @@ else:
     # Change this to something unique (e.g. mash your keyboard), and then delete this comment.
     SECRET_KEY = 'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz'
 
-# How to set heroku vars: https://devcenter.heroku.com/articles/config-vars
-if os.environ.get("HEROKU"):
-    import dj_database_url
-    DATABASES = {}
-    DATABASES['default'] = dj_database_url.config()
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(
+    default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+)
+
 
 CREATE_DEFAULT_SUPERUSER = True
 ADMIN_USERNAME = 'admin'
