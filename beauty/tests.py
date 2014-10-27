923773d821e8e359f94f533639ac0fdfa1a73c97
Juan B Cabral <jbc.develop@gmail.com>
2014-10-27 10:24:04 -0300

    fixed test for beauty, bosex and staghunt

--- beauty/tests.py
+++ beauty/tests.py
@@ -12,6 +12,11 @@ class PlayerBot(Bot):
 
         # start game
         self.submit(views.Introduction)
+        self.submit(views.QuestionOne, {
+            "training_question_1_win_pick": random.randint(0, 100),
+            "training_question_1_my_payoff": random.randint(0, 100),
+        })
+        self.submit(views.FeedbackOne)
 
         # make your guess
         self.submit(views.Guess, {"guess_value": random.randint(0, 100)})
