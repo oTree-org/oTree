4244b46bc9f5acf452a3f68af7be6ff4d7d611d4
Yurii Zolot'ko <yurii.zolotko@elance-odesk.com>
2014-10-29 10:41:06 +0700

    principal_agent game updates

--- principal_agent/models.py
+++ principal_agent/models.py
@@ -4,17 +4,35 @@ from __future__ import division
 from otree.db import models
 import otree.models
 from otree import widgets
-from otree.common import Money, money_range
-import random
+from utils import FEEDBACK_CHOICES
 # </standard imports>
 
 
 doc = """
-In Principal Agent Game, there are two players: One acts as the Agent and the other acts as the
-Principal. The Principal offers a contract to the Agent, which can be accepted or rejected.
-The game is described <a href="http://www.nottingham.ac.uk/cedex/documents/papers/2006-04.pdf" target="_blank">here</a>.
-Source code <a href="https://github.com/oTree-org/oTree/tree/master/principal_agent" target="_blank">here</a>.
+The principal offers a contract to the agent, who can decide if to reject or
+accept. The agent then chooses an effort level. The implementation is based on
+Gächter and Königstein (2006).
+
+Source code <a
+href="https://github.com/oTree-org/oTree/tree/master/principal_agent"
+target="_blank">here</a>.
 """
+
+# Gächter, Simon, and Manfred Königstein. "Design a Contract: A Simple
+# Principal-Agent Problem as a Classroom Experiment." The Journal of Economic
+# Education 40.2 (2009): 173-187.
+
+# Fehr, Ernst, Georg Kirchsteiger, and Arno Riedl. "Does fairness prevent
+# market clearing? An experimental investigation." The Quarterly Journal of
+# Economics(1993): 437-459.
+
+# Charness, Gary, Guillaume R. Frechette, and John H. Kagel. "How robust is
+# laboratory gift exchange?." Experimental Economics 7.2
+
+# http://en.wikipedia.org/wiki/Principal%E2%80%93agent_problem
+
+# Keywords: Principal-Agent Problem, Gift Exchange, Labor Market, Contract
+# Design
 
 
 class Constants:
@@ -22,34 +40,51 @@ class Constants:
     players_per_group = 2
     number_of_rounds = 1
 
-    max_fixed_payment = Money(7.00)
-    #Maxmimum absolute value for agent's fixed pay"""
+    bonus = 30
+    min_fixed_payment = -30
+    max_fixed_payment = 30
 
     # """Amount principal gets if contract is rejected"""
-    reject_principal_pay = Money(0)
+    reject_principal_pay = 0
 
-    reject_agent_pay = Money(1.00)
+    reject_agent_pay = 10
 
-    # """Total return for single unit of agent's work effort"""
-    agent_work_effort_base_return = Money(0.7)
+    agent_return_share_choices = [
+        [percent / 100, '{}%'.format(percent)]
+        for percent in range(10, 100 + 1, 10)]
 
-    agent_return_share_choices = [[percent/100, '{}%'.format(percent)] for percent in range(10, 100+1, 10)]
+    EFFORT_TO_RETURN = {
+        1: 14,
+        2: 28,
+        3: 42,
+        4: 54,
+        5: 70,
+        6: 84,
+        7: 98,
+        8: 112,
+        9: 126,
+        10: 140}
+
+    EFFORT_TO_COST = {
+        1: 0,
+        2: 4,
+        3: 8,
+        4: 12,
+        5: 18,
+        6: 24,
+        7: 32,
+        8: 40,
+        9: 50,
+        10: 60}
+
 
 def cost_from_effort(effort):
-    costs = {1: 0,
-             2: .20,
-             3: .40,
-             4: .60,
-             5: .90,
-             6: 1.20,
-             7: 1.60,
-             8: 2.00,
-             9: 2.50,
-             10: 3.00}
-    return Money(costs[effort])
+    return Constants.EFFORT_TO_COST[effort]
+
 
 def return_from_effort(effort):
-    return effort*Constants.agent_work_effort_base_return
+    return Constants.EFFORT_TO_RETURN[effort]
+
 
 class Subsession(otree.models.BaseSubsession):
 
@@ -62,35 +97,45 @@ class Group(otree.models.BaseGroup):
     subsession = models.ForeignKey(Subsession)
     # </built-in>
 
-    total_return = models.MoneyField(
-        doc="""Total return from agent's effort = [Return for single unit of agent's work effort] * [Agent's work effort]"""
+    total_return = models.PositiveIntegerField(
+        doc="""Total return from agent's effort = [Return for single unit of
+            agent's work effort] * [Agent's work effort]"""
     )
 
-    agent_fixed_pay = models.MoneyField(
-        doc="""Amount offered as fixed pay to agent"""
+    agent_fixed_pay = models.PositiveIntegerField(
+        doc="""Amount offered as fixed pay to agent""",
+        verbose_name='Fixed Payment (from %i to %i)' % (
+            Constants.min_fixed_payment, Constants.max_fixed_payment)
     )
 
     agent_return_share = models.FloatField(
         doc="""Agent's share of total return""",
+        verbose_name='Return Share',
+        widget=widgets.RadioSelectHorizontal()
     )
 
     agent_work_effort = models.PositiveIntegerField(
         doc="""Agent's work effort, [1, 10]""",
+        widget=widgets.RadioSelectHorizontal(),
     )
 
-
-    agent_work_cost = models.MoneyField(
+    agent_work_cost = models.PositiveIntegerField(
         doc="""Agent's cost of work effort"""
     )
 
     contract_accepted = models.NullBooleanField(
         doc="""Whether agent accepts proposal""",
         widget=widgets.RadioSelect(),
+        choices=(
+            (True, 'Accept'),
+            (False, 'Reject'),
+            )
     )
 
-    # choices
-    def agent_fixed_pay_choices(self):
-        return money_range(-Constants.max_fixed_payment, Constants.max_fixed_payment, 0.50)
+    def agent_fixed_pay_error_message(self, value):
+        if not Constants.min_fixed_payment <= value <=\
+                Constants.max_fixed_payment:
+            return 'Your entry is invalid.'
 
     def agent_work_effort_choices(self):
         return range(1, 10+1)
@@ -109,9 +154,12 @@ class Group(otree.models.BaseGroup):
             self.agent_work_cost = cost_from_effort(self.agent_work_effort)
             self.total_return = return_from_effort(self.agent_work_effort)
 
-            money_to_agent = self.agent_return_share*self.total_return + self.agent_fixed_pay
+            money_to_agent = self.agent_return_share * \
+                self.total_return + self.agent_fixed_pay
             agent.payoff = money_to_agent - self.agent_work_cost
             principal.payoff = self.total_return - money_to_agent
+        principal.payoff += Constants.bonus
+        agent.payoff += Constants.bonus
 
 
 class Player(otree.models.BasePlayer):
@@ -120,11 +168,16 @@ class Player(otree.models.BasePlayer):
     group = models.ForeignKey(Group, null=True)
     subsession = models.ForeignKey(Subsession)
     # </built-in>
+    training_my_payoff = models.PositiveIntegerField(
+        verbose_name='I would receive')
+    training_other_payoff = models.PositiveIntegerField(
+        verbose_name='Participant B would receive')
+    feedback = models.PositiveIntegerField(
+        choices=FEEDBACK_CHOICES, widget=widgets.RadioSelectHorizontal(),
+        verbose_name='How well do you think this sample game was implemented?')
 
     def role(self):
         if self.id_in_group == 1:
             return 'principal'
         if self.id_in_group == 2:
             return 'agent'
-
-
