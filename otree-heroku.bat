fb36adfc89386d86869289596374ce6cb9d21f27
wickens <c.wickens@gmail.com>
2014-11-27 23:30:13 +0900

    added otree-heroku shell script

---
+++ otree-heroku.bat
@@ -0,0 +1,13 @@
+:: A wrapper around the ./otree-heroku script for windows
+::
+:: On Windows open the `cmd` command line terminal, then go to the directory
+:: that this file is in with the `cd` command, e.g.
+::
+::   cd C:\Projects\oTree
+::
+:: Then type:
+::
+::   otree-heroku help
+::
+
+@python otree-heroku %*
