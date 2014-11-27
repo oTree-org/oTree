0467d5717591dded09b56b5e2ad901b050f0a9f7
Gregor Müllegger <gregor@muellegger.de>
2014-11-27 10:50:14 +0100

    Moving ./otree -> ./otree-heroku and making ./otree a simple replacement for manage.py

--- manage.py
+++ manage.py
@@ -1,20 +1,10 @@
 #!/usr/bin/env python
-"""
-Simple wrapper around ./otree
-
-This file exists to provide tools with a file called manage.py since services
-like heroku expect the existence of such. As enduser you can and should always
-use the ./otree script.
-"""
-
 import os
 import sys
-import subprocess
 
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-if __name__ == '__main__':
-    base_path = os.path.dirname(os.path.abspath(__file__))
-    otree_script = os.path.join(base_path, 'otree')
+    from django.core.management import execute_from_command_line
 
-    return_code = subprocess.call([otree_script] + sys.argv[1:])
-    sys.exit(return_code)
+    execute_from_command_line(sys.argv)
