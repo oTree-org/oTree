8450fc71c3498900a202020c044fa29a6b2cb318
wickens <c.wickens@gmail.com>
2015-07-30 23:27:16 +0900

    move wsgi.py into core

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn wsgi
+web: gunicorn otree.wsgi
 worker: python manage.py celery worker --app=otree.celery.app:app --loglevel=INFO
--- wsgi.py
+++
@@ -1,8 +0,0 @@
-import os
-#my comment here
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
-
-from django.core.wsgi import get_wsgi_application
-
-from dj_static import Cling
-application = Cling(get_wsgi_application())
