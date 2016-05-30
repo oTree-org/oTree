e2f3def86c98147cbc5e1c3b5810d0d7089b0eb1
Alex Guzey <alex@guzey.com>
2016-05-30 17:31:30 +0300

    add info re rounds for players

--- reform/views.py
+++ reform/views.py
@@ -39,14 +39,16 @@ class PreOverthrow(Page):
     def vars_for_template(self):
         if self.subsession.round_number == 1:
             return {
-                'reformed_this_round': self.player.participant.vars['reformed_this_round']
+                'reformed_this_round': self.player.participant.vars['reformed_this_round'],
+                'current_round': self.subsession.round_number
             }
         else:
             return {
                 'total_approvals': self.group.approvals_in_previous_round(),
                 'reformed_this_round': self.player.participant.vars['reformed_this_round'],
                 'player_payoff_in_previous_round': self.player.in_round(self.subsession.round_number-1).payoff,
-                'player_payoff': sum([p.payoff for p in self.player.in_previous_rounds()])
+                'player_payoff': sum([p.payoff for p in self.player.in_previous_rounds()]),
+                'current_round': self.subsession.round_number,
             }
 
     form_model = models.Player
