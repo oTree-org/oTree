aac716aa9110e8e932e04fddf9dee3325744767f
oTree-org <chris@otree.org>
2016-08-30 14:37:31 +0900

    more asserts for real_effort

--- real_effort/tests.py
+++ real_effort/tests.py
@@ -14,7 +14,8 @@ class PlayerBot(Bot):
         yield SubmissionMustFail(views.Transcribe, {'transcribed_text': 'foo'})
 
         transcription = Constants.reference_texts[self.subsession.round_number - 1]
-        if Constants.allowed_error_rates[self.subsession.round_number - 1] > 0:
+        add_char = Constants.allowed_error_rates[self.subsession.round_number - 1] > 0
+        if add_char:
             # add a 1-char error, should still be fine
             transcription += 'a'
 
@@ -28,7 +29,15 @@ class PlayerBot(Bot):
         ]:
             assert value != None
 
+        if add_char:
+            assert self.player.levenshtein_distance == 1
+        else:
+            assert self.player.levenshtein_distance == 0
+
         if self.subsession.round_number == Constants.num_rounds:
             # final page should print lengths of all reference texts
             for ref_text in Constants.reference_texts:
                 assert str(len(ref_text)) in self.html
+
+            # because of add_char, there should be an error in at least 1 round
+            assert any(p.levenshtein_distance > 0 for p in self.player.in_all_rounds())
