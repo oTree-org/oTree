22c3973ebd7722414e945f51832d72d4b2c3b709
oTree-org <chris@otree.org>
2016-10-11 16:27:41 +0900

    .

--- survey/models.py
+++ survey/models.py
@@ -36,6 +36,25 @@ class Player(BasePlayer):
                                 verbose_name='What is your gender?',
                                 widget=widgets.RadioSelect())
 
-    crt_bat = models.PositiveIntegerField()
-    crt_widget = models.PositiveIntegerField()
-    crt_lake = models.PositiveIntegerField()
+    crt_bat = models.PositiveIntegerField(
+        verbose_name='''
+        A bat and a ball cost 22 dollars in total.
+        The bat costs 20 dollars more than the ball.
+        How many dollars does the ball cost?'''
+    )
+
+    crt_widget = models.PositiveIntegerField(
+        verbose_name='''
+        "If it takes 5 machines 5 minutes to make 5 widgets,
+        how many minutes would it take 100 machines to make 100 widgets?"
+        '''
+    )
+
+    crt_lake = models.PositiveIntegerField(
+        verbose_name='''
+        In a lake, there is a patch of lily pads.
+        Every day, the patch doubles in size.
+        If it takes 48 days for the patch to cover the entire lake,
+        how many days would it take for the patch to cover half of the lake?
+        '''
+    )
