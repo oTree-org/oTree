38b876bec9d02e1f825fe95a4eb0a910491928df
oTree-org <chris@otree.org>
2019-09-08 10:57:15 +0900

    deactivate session configs

--- settings.py
+++ settings.py
@@ -30,36 +30,7 @@ SESSION_CONFIGS = [
         num_demo_participants=1,
         app_sequence=['survey', 'payment_info']
     ),
-    dict(name='trust', display_name="Trust Game", num_demo_participants=2, app_sequence=['trust', 'payment_info']),
-    dict(name='prisoner', display_name="Prisoner's Dilemma", num_demo_participants=2,
-         app_sequence=['prisoner', 'payment_info']),
-    dict(name='volunteer_dilemma', display_name="Volunteer's Dilemma", num_demo_participants=3,
-         app_sequence=['volunteer_dilemma', 'payment_info']),
-    dict(name='cournot', display_name="Cournot Competition", num_demo_participants=2, app_sequence=[
-        'cournot', 'payment_info'
-    ]),
-    dict(name='dictator', display_name="Dictator Game", num_demo_participants=2,
-         app_sequence=['dictator', 'payment_info']),
-    dict(name='matching_pennies', display_name="Matching Pennies", num_demo_participants=2, app_sequence=[
-        'matching_pennies',
-    ]),
-    dict(name='traveler_dilemma', display_name="Traveler's Dilemma", num_demo_participants=2,
-         app_sequence=['traveler_dilemma', 'payment_info']),
-    dict(name='bargaining', display_name="Bargaining Game", num_demo_participants=2,
-         app_sequence=['bargaining', 'payment_info']),
-    dict(name='common_value_auction', display_name="Common Value Auction", num_demo_participants=3,
-         app_sequence=['common_value_auction', 'payment_info']),
-    dict(name='bertrand', display_name="Bertrand Competition", num_demo_participants=2, app_sequence=[
-        'bertrand', 'payment_info'
-    ]),
-    dict(name='public_goods_simple', display_name="Public Goods (simple version from tutorial)",
-         num_demo_participants=3, app_sequence=['public_goods_simple', 'payment_info']),
-    dict(name='trust_simple', display_name="Trust Game (simple version from tutorial)", num_demo_participants=2,
-         app_sequence=['trust_simple']),
 ]
-
-# see the end of this file for the inactive session configs
-
 
 # ISO-639 code
 # for example: de, fr, ja, ko, zh-hans
@@ -89,4 +60,29 @@ SECRET_KEY = '6lertt4wlb09zj@4wyuy-p-6)i$vh!ljwx&r9bti6kgw54k-h8'
 INSTALLED_APPS = ['otree']
 
 # inactive session configs
-
+# dict(name='trust', display_name="Trust Game", num_demo_participants=2, app_sequence=['trust', 'payment_info']),
+# dict(name='prisoner', display_name="Prisoner's Dilemma", num_demo_participants=2,
+#      app_sequence=['prisoner', 'payment_info']),
+# dict(name='volunteer_dilemma', display_name="Volunteer's Dilemma", num_demo_participants=3,
+#      app_sequence=['volunteer_dilemma', 'payment_info']),
+# dict(name='cournot', display_name="Cournot Competition", num_demo_participants=2, app_sequence=[
+#     'cournot', 'payment_info'
+# ]),
+# dict(name='dictator', display_name="Dictator Game", num_demo_participants=2,
+#      app_sequence=['dictator', 'payment_info']),
+# dict(name='matching_pennies', display_name="Matching Pennies", num_demo_participants=2, app_sequence=[
+#     'matching_pennies',
+# ]),
+# dict(name='traveler_dilemma', display_name="Traveler's Dilemma", num_demo_participants=2,
+#      app_sequence=['traveler_dilemma', 'payment_info']),
+# dict(name='bargaining', display_name="Bargaining Game", num_demo_participants=2,
+#      app_sequence=['bargaining', 'payment_info']),
+# dict(name='common_value_auction', display_name="Common Value Auction", num_demo_participants=3,
+#      app_sequence=['common_value_auction', 'payment_info']),
+# dict(name='bertrand', display_name="Bertrand Competition", num_demo_participants=2, app_sequence=[
+#     'bertrand', 'payment_info'
+# ]),
+# dict(name='public_goods_simple', display_name="Public Goods (simple version from tutorial)",
+#      num_demo_participants=3, app_sequence=['public_goods_simple', 'payment_info']),
+# dict(name='trust_simple', display_name="Trust Game (simple version from tutorial)", num_demo_participants=2,
+#      app_sequence=['trust_simple']),
