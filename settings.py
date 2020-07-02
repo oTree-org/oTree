515183d5744beb270271ebd8fc874b201f7c4628
oTree-org <chris@otree.org>
2020-07-02 14:55:58 +0800

    move SESSION_CONFIGS to the top of settings.py

--- settings.py
+++ settings.py
@@ -1,14 +1,5 @@
 from os import environ
 
-# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
-# in SESSION_CONFIGS, except those that explicitly override it.
-# the session config can be accessed from methods in your apps as self.session.config,
-# e.g. self.session.config['participation_fee']
-
-SESSION_CONFIG_DEFAULTS = dict(
-    real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
-)
-
 SESSION_CONFIGS = [
     dict(
         name='public_goods',
@@ -101,6 +92,15 @@ SESSION_CONFIGS = [
         app_sequence=['trust_simple'],
     ),
 ]
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+
+SESSION_CONFIG_DEFAULTS = dict(
+    real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
+)
 
 
 # ISO-639 code
