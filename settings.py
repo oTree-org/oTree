1e38b8e5c57d57e17c9ee8084ad6edfb081938c7
dcthomas4679 <dcthomas@gmail.com>
2020-09-01 00:05:05 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -20,6 +20,73 @@ SESSION_CONFIGS = [
         num_demo_participants=1,
         app_sequence=['survey', 'payment_info'],
     ),
+    dict(
+        name='trust',
+        display_name="Trust Game",
+        num_demo_participants=2,
+        app_sequence=['trust', 'payment_info']
+    ),
+    dict(
+         name='trust',
+         display_name="Trust Game",
+         num_demo_participants=2,
+         app_sequence=['trust', 'payment_info']
+     ),
+    dict(name='prisoner',
+        display_name="Prisoner's Dilemma",
+        num_demo_participants=2,
+        app_sequence=['prisoner', 'payment_info']
+    ),
+    dict(name='volunteer_dilemma',
+        display_name="Volunteer's Dilemma",
+        num_demo_participants=3,
+        app_sequence=['volunteer_dilemma', 'payment_info']
+    ),
+    dict(name='cournot',
+        display_name="Cournot Competition",
+        num_demo_participants=2,
+        app_sequence=['cournot', 'payment_info']
+    ),
+    dict(name='dictator',
+        display_name="Dictator Game",
+        num_demo_participants=2,
+        app_sequence=['dictator', 'payment_info']
+    ),
+    dict(name='matching_pennies',
+        display_name="Matching Pennies",
+        num_demo_participants=2,
+        app_sequence=['matching_pennies',]
+    ),
+    dict(name='traveler_dilemma',
+        display_name="Traveler's Dilemma",
+        num_demo_participants=2,
+        app_sequence=['traveler_dilemma', 'payment_info']
+    ),
+    dict(name='bargaining',
+        display_name="Bargaining Game",
+        num_demo_participants=2,
+        app_sequence=['bargaining', 'payment_info']
+    ),
+    dict(name='common_value_auction',
+        display_name="Common Value Auction",
+        num_demo_participants=3,
+        app_sequence=['common_value_auction', 'payment_info']
+    ),
+    dict(name='bertrand',
+        display_name="Bertrand Competition",
+        num_demo_participants=2,
+        app_sequence=['bertrand', 'payment_info']
+    ),
+    dict(name='public_goods_simple',
+        display_name="Public Goods (simple version from tutorial)",
+        num_demo_participants=3,
+        app_sequence=['public_goods_simple', 'payment_info']
+    ),
+    dict(name='trust_simple',
+        display_name="Trust Game (simple version from tutorial)",
+        num_demo_participants=2,
+        app_sequence=['trust_simple']
+    ),
 ]
 
 # if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
@@ -41,16 +108,16 @@ USE_POINTS = True
 
 ROOMS = [
     dict(
-        name='econ101',
-        display_name='Econ 101 class',
-        participant_label_file='_rooms/econ101.txt',
+        name='econ371',
+        display_name='Econ econ371 class',
+        participant_label_file='_rooms/Econ371.txt',
     ),
     dict(name='live_demo', display_name='Room for live demo (no participant labels)'),
 ]
 
-ADMIN_USERNAME = 'admin'
+ADMIN_USERNAME = 'dcthomas4679'
 # for security, best to set admin password in an environment variable
-ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+ADMIN_PASSWORD = 'BSU$Fubman4679'
 
 DEMO_PAGE_INTRO_HTML = """
 Here are some oTree games.
@@ -61,30 +128,5 @@ SECRET_KEY = '{{ secret_key }}'
 
 INSTALLED_APPS = ['otree']
 
-# inactive session configs
-# dict(name='trust', display_name="Trust Game", num_demo_participants=2, app_sequence=['trust', 'payment_info']),
-# dict(name='prisoner', display_name="Prisoner's Dilemma", num_demo_participants=2,
-#      app_sequence=['prisoner', 'payment_info']),
-# dict(name='volunteer_dilemma', display_name="Volunteer's Dilemma", num_demo_participants=3,
-#      app_sequence=['volunteer_dilemma', 'payment_info']),
-# dict(name='cournot', display_name="Cournot Competition", num_demo_participants=2, app_sequence=[
-#     'cournot', 'payment_info'
-# ]),
-# dict(name='dictator', display_name="Dictator Game", num_demo_participants=2,
-#      app_sequence=['dictator', 'payment_info']),
-# dict(name='matching_pennies', display_name="Matching Pennies", num_demo_participants=2, app_sequence=[
-#     'matching_pennies',
-# ]),
-# dict(name='traveler_dilemma', display_name="Traveler's Dilemma", num_demo_participants=2,
-#      app_sequence=['traveler_dilemma', 'payment_info']),
-# dict(name='bargaining', display_name="Bargaining Game", num_demo_participants=2,
-#      app_sequence=['bargaining', 'payment_info']),
-# dict(name='common_value_auction', display_name="Common Value Auction", num_demo_participants=3,
-#      app_sequence=['common_value_auction', 'payment_info']),
-# dict(name='bertrand', display_name="Bertrand Competition", num_demo_participants=2, app_sequence=[
-#     'bertrand', 'payment_info'
-# ]),
-# dict(name='public_goods_simple', display_name="Public Goods (simple version from tutorial)",
-#      num_demo_participants=3, app_sequence=['public_goods_simple', 'payment_info']),
-# dict(name='trust_simple', display_name="Trust Game (simple version from tutorial)", num_demo_participants=2,
-#      app_sequence=['trust_simple']),
+#inactive session configs
+ 
