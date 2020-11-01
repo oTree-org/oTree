b489b74c9f054f8a7d27ea9ab3fe2fdc6e7c5378
oTree-org <chris@otree.org>
2020-11-01 08:25:00 +0800

    remove inactive session configs for space

--- settings.py
+++ settings.py
@@ -60,31 +60,3 @@ Here are some oTree games.
 SECRET_KEY = '{{ secret_key }}'
 
 INSTALLED_APPS = ['otree']
-
-# inactive session configs
-# dict(name='trust', display_name="Trust Game", num_demo_participants=2, app_sequence=['trust', 'payment_info']),
-# dict(name='prisoner', display_name="Prisoner's Dilemma", num_demo_participants=2,
-#      app_sequence=['prisoner', 'payment_info']),
-# dict(name='volunteer_dilemma', display_name="Volunteer's Dilemma", num_demo_participants=3,
-#      app_sequence=['volunteer_dilemma', 'payment_info']),
-# dict(name='cournot', display_name="Cournot Competition", num_demo_participants=2, app_sequence=[
-#     'cournot', 'payment_info'
-# ]),
-# dict(name='dictator', display_name="Dictator Game", num_demo_participants=2,
-#      app_sequence=['dictator', 'payment_info']),
-# dict(name='matching_pennies', display_name="Matching Pennies", num_demo_participants=2, app_sequence=[
-#     'matching_pennies',
-# ]),
-# dict(name='traveler_dilemma', display_name="Traveler's Dilemma", num_demo_participants=2,
-#      app_sequence=['traveler_dilemma', 'payment_info']),
-# dict(name='bargaining', display_name="Bargaining Game", num_demo_participants=2,
-#      app_sequence=['bargaining', 'payment_info']),
-# dict(name='common_value_auction', display_name="Common Value Auction", num_demo_participants=3,
-#      app_sequence=['common_value_auction', 'payment_info']),
-# dict(name='bertrand', display_name="Bertrand Competition", num_demo_participants=2, app_sequence=[
-#     'bertrand', 'payment_info'
-# ]),
-# dict(name='public_goods_simple', display_name="Public Goods (simple version from tutorial)",
-#      num_demo_participants=3, app_sequence=['public_goods_simple', 'payment_info']),
-# dict(name='trust_simple', display_name="Trust Game (simple version from tutorial)", num_demo_participants=2,
-#      app_sequence=['trust_simple']),
