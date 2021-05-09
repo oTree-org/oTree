f12b67f8eb059ecac0d1f251ed0857375da2c6f3
oTree-org <chris@otree.org>
2021-05-09 19:23:07 +0800

    add PARTICIPANT_FIELDS

--- settings.py
+++ settings.py
@@ -27,6 +27,9 @@ SESSION_CONFIG_DEFAULTS = dict(
     real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
 )
 
+PARTICIPANT_FIELDS = []
+SESSION_FIELDS = []
+
 # ISO-639 code
 # for example: de, fr, ja, ko, zh-hans
 LANGUAGE_CODE = 'en'
